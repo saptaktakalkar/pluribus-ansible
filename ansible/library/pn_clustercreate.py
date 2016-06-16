018fa62246ef4be9c75712a660cccb5f894eea1b
Ramanuj Jaju <ramanuj.jaju@pluribusnetworks.com>
2016-06-16 08:54:57 -0700

    Update pn_clustercreate.py

--- ansible/library/pn_clustercreate.py
+++ ansible/library/pn_clustercreate.py
@@ -1,6 +1,77 @@
 #!/usr/bin/python
 # Test PN CLI cluster-create
 
+
+DOCUMENTATION = """
+---
+module: pn_clustercreate
+author: "Pluribus Networks"
+short_description: CLI command to create a cluster
+description:
+  - Execute cluster-create command. 
+  - Requires cluster name:
+  	- Alphanumeric characters
+  	- Special characters like: _ 
+  - Requires 2 nodes, cluster-node-1 and cluster-node-2
+options:
+  pn_clustercommand:
+    description:
+      - The C(pn_clustercommand) takes the cluster-create command as value.
+        Create a new cluster for high availability (HA) in a fabric
+    required: true
+    type: str
+  pn_clustername:
+    description:
+      - The C(pn_clustername) takes a valid name for cluster configuration.
+    required: true
+    type: str
+  pn_clusternode1:
+    description:
+      - name for cluster-node 1
+    required: true
+    type: str
+  pn_clusternode2:
+    description:
+      - name for cluster-node 2
+    required: true
+    type: str
+  pn_clustervalidate:
+    description:
+      - validate the cluster link
+    required: false
+    type: bool
+    default: true
+  pn_quiet:
+    description:
+      - The C(pn_quiet) option to enable or disable the system bootup message
+    required: false
+    type: bool
+    default: true
+"""
+
+EXAMPLES = """
+- name: create spine cluster CLI command
+  pn_clustercreate: pn_clustercommand='cluster-create' pn_clustername='spine-cluster' pn_clusternode1='spine01' pn_clusternode2='spine02' pn_clustervalidate=True pn_quiet=True
+- name: create leaf cluster CLI command
+  pn_clustercreate: pn_clustercommand='cluster-create' pn_clustername='leaf-cluster' pn_clusternode1='leaf01' pn_clusternode2='leaf02' pn_clustervalidate=True pn_quiet=True
+"""
+
+RETURN = """
+showcmd:
+  description: the CLI command run on the target node(s).
+stdout:
+  description: the set of responses from the show command.
+  returned: always
+  type: list
+stdout_lines:
+  description: the value of stdout split into a list.
+  returned: always
+  type: list
+stderr:
+  description: the set of error responses from the show command.
+  returned: on error
+  type: list
+"""
 import subprocess
 import shlex
 import json
@@ -13,7 +84,7 @@ def main():
 			pn_clustername = dict(required=True, type='str'),
 			pn_clusternode1 = dict(required=True, type='str'),
 			pn_clusternode2 = dict(required=True, type='str'),	
-			pn_clustervalidate = dict(type='bool'),
+			pn_clustervalidate = dict(default=True, type='bool'),
 			pn_quiet = dict(default=True, type='bool')
 			)
 	)
@@ -30,7 +101,7 @@ def main():
 	else:
 		cli = "/usr/bin/cli " 
 	
-	if clustercommand == " ":
+	if clustercommand != "cluster-create":
 		module.fail_json(msg="Invalid command")
 
 
