43fbf881c6b68b208bff5fa30d630a2fc30e8e3b
Ramanuj Jaju <ramanuj.jaju@pluribusnetworks.com>
2016-06-16 09:14:58 -0700

    Update pn_clusterdelete.py

--- ansible/library/pn_clusterdelete.py
+++ ansible/library/pn_clusterdelete.py
@@ -1,6 +1,59 @@
 #!/usr/bin/python
 # Test PN CLI cluster-delete
 
+DOCUMENTATION = """
+---
+module: pn_clusterdelete
+author: "Pluribus Networks"
+short_description: CLI command to delete a cluster.
+description:
+  - Execute cluster-delete command. Delete a cluster for high availability (HA) in a fabric.
+options:
+  pn_clustercommand:
+    description:
+      - The C(pn_clustercommand) takes the cluster-delete command as value.
+        Delete a cluster for high availability (HA) in a fabric.
+    required: true
+    type: str
+  pn_clustername:
+    description:
+      - The C(pn_clustername) takes a valid name for cluster configuration.
+        Name of the cluster to be deleted.
+    required: true
+    type: str
+  pn_quiet:
+    description:
+      - The C(pn_quiet) option to enable or disable the system bootup message
+    required: false
+    type: bool
+    default: true
+"""
+
+EXAMPLES = """
+- name: create spine cluster CLI command
+  pn_clustercreate: pn_clustercommand='cluster-delete' pn_clustername='spine-cluster' pn_quiet=True
+- name: create leaf cluster CLI command
+  pn_clustercreate: pn_clustercommand='cluster-delete' pn_clustername='leaf-cluster' pn_quiet=True
+"""
+
+RETURN = """
+clustercmd:
+  description: the CLI command run on the target node(s).
+stdout:
+  description: the set of responses from the cluster command.
+  returned: always
+  type: list
+stdout_lines:
+  description: the value of stdout split into a list.
+  returned: always
+  type: list
+stderr:
+  description: the set of error responses from the show command.
+  returned: on error
+  type: list
+"""
+
+
 import subprocess
 import shlex
 import json
