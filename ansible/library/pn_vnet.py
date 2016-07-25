b3759405a25d3ce75f1bfe53b0f41cf705faa2fb
Ramanuj Jaju <ramanuj.jaju@pluribusnetworks.com>
2016-07-25 09:33:25 -0700

    Removed return code 'rc'

--- ansible/library/pn_vnet.py
+++ ansible/library/pn_vnet.py
@@ -119,10 +119,6 @@ stderr:
   description: the set of error responses from the vnet command.
   returned: on error
   type: list
-rc:
-  description: return code of the module.
-  returned: 0 on success, 1 on error
-  type: int
 changed:
   description: Indicates whether the CLI caused changes on the target.
   returned: always
@@ -239,7 +235,6 @@ def main():
         module.exit_json(
             command=cli,
             stderr=err.rstrip("\r\n"),
-            rc=1,
             changed=False
         )
 
@@ -247,10 +242,8 @@ def main():
         module.exit_json(
             command=cli,
             stdout=out.rstrip("\r\n"),
-            rc=0,
             changed=True
         )
-
 
 # AnsibleModule boilerplate
 from ansible.module_utils.basic import AnsibleModule
