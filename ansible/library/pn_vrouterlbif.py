b48d250753c25908c917502504984746c4f9ee25
Ramanuj Jaju <ramanuj.jaju@pluribusnetworks.com>
2016-07-25 09:37:17 -0700

    Removed return code 'rc'

--- ansible/library/pn_vrouterlbif.py
+++ ansible/library/pn_vrouterlbif.py
@@ -91,10 +91,6 @@ stderr:
   description: the set of error responses from the vrouterlb command.
   returned: on error
   type: list
-rc:
-  description: return code of the module.
-  returned: 0 on success, 1 on error
-  type: int
 changed:
   description: Indicates whether the CLI caused changes on the target.
   returned: always
@@ -171,7 +167,6 @@ def main():
         module.exit_json(
             command=cli,
             stderr=err.rstrip("\r\n"),
-            rc=1,
             changed=False
         )
 
@@ -179,7 +174,6 @@ def main():
         module.exit_json(
             command=cli,
             stdout=out.rstrip("\r\n"),
-            rc=0,
             changed=True
         )
 
