29c75bdc06b92a9436ce1db2f22733072f88c87f
Ramanuj Jaju <ramanuj.jaju@pluribusnetworks.com>
2016-07-25 09:28:32 -0700

    Removed return code 'rc'

--- ansible/library/pn_ospfarea.py
+++ ansible/library/pn_ospfarea.py
@@ -95,10 +95,6 @@ stderr:
   description: the set of error responses from the ospf command.
   returned: on error
   type: list
-rc:
-  description: return code of the module.
-  returned: 0 on success, 1 on error
-  type: int
 changed:
   description: Indicates whether the CLI caused changes on the target.
   returned: always
@@ -188,7 +184,6 @@ def main():
         module.exit_json(
             command=cli,
             stderr=err.rstrip("\r\n"),
-            rc=1,
             changed=False
         )
 
@@ -196,7 +191,6 @@ def main():
         module.exit_json(
             command=cli,
             stdout=out.rstrip("\r\n"),
-            rc=0,
             changed=True
         )
 
