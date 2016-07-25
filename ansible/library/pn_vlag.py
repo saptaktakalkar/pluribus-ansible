112c2d8fb56a503a8cafb8204919af0954044e07
Ramanuj Jaju <ramanuj.jaju@pluribusnetworks.com>
2016-07-25 09:31:41 -0700

    Removed return code 'rc'

--- ansible/library/pn_vlag.py
+++ ansible/library/pn_vlag.py
@@ -128,10 +128,6 @@ stderr:
   description: the set of error responses from the vlag command.
   returned: on error
   type: list
-rc:
-  description: return code of the module.
-  returned: 0 on success, 1 on error
-  type: int
 changed:
   description: Indicates whether the CLI caused changes on the target.
   returned: always
@@ -248,7 +244,6 @@ def main():
         module.exit_json(
             command=cli,
             stderr=err.rstrip("\r\n"),
-            rc=1,
             changed=False
         )
 
@@ -256,10 +251,8 @@ def main():
         module.exit_json(
             command=cli,
             stdout=out.rstrip("\r\n"),
-            rc=0,
             changed=True
         )
-
 
 # AnsibleModule boilerplate
 from ansible.module_utils.basic import AnsibleModule
