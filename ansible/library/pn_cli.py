b34ccddc7775a06f28859e86199a7b2add630e56
ramanujjaju <ramanuj.jaju@pluribusnetworks.com>
2016-08-02 11:07:08 -0700

    Update docs and license

--- ansible/library/pn_cli.py
+++ ansible/library/pn_cli.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
-""" This is a basic module to run any PN CLI commands. 
+""" This is a basic module to run any PN CLI commands.
     The CLI command should be provide to 'pn_command' parameter.
 """
+
+# Copyright 2016 Pluribus Networks
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import subprocess
 import shlex
@@ -27,15 +41,15 @@ def main():
 
     # Building the CLI command string
     cli = '/usr/bin/cli'
-    
+
     if quiet is True:
         cli += ' --quiet '
-    
+
     cli += ' --user %s:%s ' % (cliusername, clipassword)
-    
+
     if cliswitch:
         cli += ' switch-local ' if cliswitch == 'local' else ' switch ' + cliswitch
-    
+
     cli += ' ' + command
 
     cli_command = shlex.split(cli)
