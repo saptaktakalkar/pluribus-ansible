1918a725e754fa44e69c8e7ed25be09ebc0fe0f5
gaurav bajaj <gaurav.bajaj@calsoftinc.com>
2017-04-03 02:58:35 -0700

    The latest changes in l2 modules with json output

--- ansible/library/pn_ztp_vrrp_l2_csv.py
+++ ansible/library/pn_ztp_vrrp_l2_csv.py
@@ -134,7 +134,7 @@ def run_cli(module, cli):
             summary=results,
             task='CLI command to configure VRRP - Layer 2 Setup',
             stderr=err.strip(),
-            msg='Operation Failed: ' + str(cli),
+            msg='L2 VRRP configuration failed',
             changed=False
         )
     else:
@@ -382,7 +382,7 @@ def main():
     # Exit the module and return the required JSON.
     module.exit_json(
         unreachable=False,
-        msg = 'Module executed successfully',
+        msg = 'L2 VRRP configuration executed successfully',
         summary=results,
         exception='',
         task='CLI command to configure VRRP - Layer 2 Setup',
