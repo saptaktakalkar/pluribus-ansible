9b9ceb40de31394d62d1c1cc0a059ad816bb7a0d
Saptak Takalkar <saptak.takalkar@calsoftinc.com>
2017-04-24 01:05:08 -0700

    Added hosts validation and updated csv validation

---
+++ ansible/library/pn_hosts_file_validation.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python
+""" PN Hosts File Validation """
+
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import socket
+
+from ansible.module_utils.basic import AnsibleModule
+
+DOCUMENTATION = """
+---
+module: pn_hosts_file_validation
+author: 'Pluribus Networks (devops@pluribusnetworks.com)'
+short_description: Module to validate Ansible hosts/inventory file.
+description:
+    HOSTS file format: switch_name ansible_hosts=ip_address
+    Every row in the hosts file should have above 2 elements.
+    Hosts file should not be empty. This module validates the given hosts file.
+options:
+    pn_hosts_file_data:
+      description: String containing Hosts file data parsed.
+      required: True
+      type: str
+"""
+
+EXAMPLES = """
+- name: Validate hosts file
+  pn_hosts_file_validation:
+    pn_hosts_file_data: "{{ lookup('file', '{{ hosts_file }}') }}"
+"""
+
+RETURN = """
+summary:
+  description: It contains output of each validation.
+  returned: always
+  type: str
+changed:
+  description: Indicates whether the validation caused changes on the target.
+  returned: always
+  type: bool
+unreachable:
+  description: Empty string.
+  returned: always
+  type: bool
+failed:
+  description: Indicates if hosts file validation failed or not.
+  returned: always
+  type: bool
+exception:
+  description: Empty string.
+  returned: always
+  type: str
+task:
+  description: Name of the task getting executed.
+  returned: always
+  type: str
+msg:
+  description: Indicates whether hosts file is valid or invalid.
+  returned: always
+  type: str
+"""
+
+
+def main():
+    """ This section is for arguments parsing """
+    module = AnsibleModule(
+        argument_spec=dict(
+            pn_hosts_file_data=dict(required=True, type='str'),
+        )
+    )
+
+    output = ''
+    line_count = 0
+    switch_names = []
+    host_ips = []
+    connection_str = "ansible_user=\"{{ SSH_USER }}\" "
+    connection_str += "ansible_ssh_pass=\"{{ SSH_PASS }}\" "
+    connection_str += "ansible_become_pass=\"{{ SSH_PASS }}\""
+    hosts_file_data = module.params['pn_hosts_file_data']
+
+    if hosts_file_data:
+        hosts_file_data = hosts_file_data.split('\n')
+        for row in hosts_file_data:
+            line_count += 1
+            if not row or row.startswith('#'):
+                # Skip blank lines and comments which starts with '#'
+                continue
+            else:
+                elements = row.split(" ", 2)
+                # [spine] and [leaf] group validation
+                if len(elements) == 1:
+                    if '[spine]' in elements:
+                        pass
+                    elif '[leaf]' in elements:
+                        pass
+                    else:
+                        output += 'Invalid entry at line number {}\n'.format(
+                            line_count
+                        )
+                elif len(elements) == 3:
+                    switch = elements[0]
+                    ip = elements[1]
+
+                    # Switch name validation
+                    if switch not in switch_names:
+                        switch_names.append(switch)
+                        if switch.isdigit():
+                            output += 'Invalid SWITCH NAME {} '.format(switch)
+                            output += 'at line number {}\n'.format(line_count)
+                    else:
+                        output += 'Duplicate SWITCH NAME {} '.format(switch)
+                        output += 'at line number {}\n'.format(line_count)
+
+                    # Host ip validation
+                    ip_host = ip.split('=')
+                    host = ip_host[0]
+                    ip = ip_host[1]
+
+                    if host != 'ansible_host':
+                        output += 'Invalid spelling of ansible_host '
+                        output += 'at line number {}\n'.format(line_count)
+
+                    if ip not in host_ips:
+                        host_ips.append(ip)
+                        try:
+                            dot_count = ip.count('.')
+                            if dot_count != 3:
+                                raise socket.error
+
+                            socket.inet_aton(ip)
+                        except socket.error:
+                            output += 'Invalid HOST IP {} '.format(ip)
+                            output += 'at line number {}\n'.format(line_count)
+                    else:
+                        output += 'Duplicate HOST IP {} '.format(ip)
+                        output += 'at line number {}\n'.format(line_count)
+
+                    # Ansible connection variable validation
+                    if elements[2] != connection_str:
+                        output += 'Invalid Ansible connection variables '
+                        output += 'at line number {}\n'.format(line_count)
+                else:
+                    output += 'Invalid entry at line number {}\n'.format(
+                        line_count
+                    )
+    else:
+        output += 'Hosts file should not be empty\n'
+
+    if not output:
+        msg = 'Valid hosts file'
+        failed_flag = False
+    else:
+        msg = 'Invalid hosts file'
+        failed_flag = True
+
+    module.exit_json(
+        unreachable=False,
+        msg=output,
+        summary=msg,
+        exception='',
+        failed=failed_flag,
+        changed=False,
+        task='Validate hosts file'
+    )
+
+if __name__ == '__main__':
+    main()
+
